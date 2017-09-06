e0ff896c9e097c9d2119a3bb77e9a4b0e7c7b3f8
NicolasAramayoGit <aramayo.fabian@gmail.com>
2017-09-06 00:24:02 -0300

    add file

---
+++ LaboratorioIII/Clase3/ClaseJavascript/js/index.js
@@ -0,0 +1,12 @@
+var a = 1; //number
+
+alert(a);
+
+a = function(){alert("soy una funcion");}
+
+// ARRAYS
+var miarray = ["Enero",1,"Marzo"];
+
+if (typeof(miarray[0]==="string")) {
+    alert("soy un string");
+}
