f2c10efaa1b7b6964a5a5ad78e0a429f9557b8fb
nlsargit <aramayo.fabian@gmail.com>
2017-11-10 20:37:15 -0300

    JS: map, filter y reduce
    Tarea de consultas terminadas.

--- LaboratorioIII/Clase8/ClaseMapFilterReduce/mapReduceFilter/Consultas/scripts.js
+++ LaboratorioIII/Clase8/ClaseMapFilterReduce/mapReduceFilter/Consultas/scripts.js
@@ -1,4 +1,4 @@
-//console.log(data);
+console.log(data);
 
 /*
     realizar las operaciones usando los metodos map,  reduce y filter y combinaciones entre ellos
@@ -19,7 +19,7 @@ soluciones.usuariosFemeninos = function(usuarios){
     });
 }
 
-//console.log(soluciones.usuariosFemeninos(data));
+console.log(soluciones.usuariosFemeninos(data));
 
 // Retornar un array de strings (el email de los usuarios de sexo masculino)
 
@@ -33,23 +33,40 @@ soluciones.mailsVarones = function(usuarios){
     });
 }
 
-//console.log(soluciones.mailsVarones(data));
+console.log(soluciones.mailsVarones(data));
 
 // Retornar un array de objetos que solo contengan las claves nombre, email y edad, de todos los usuarios mayores que 'edad'
 
 soluciones.usuariosMayores = function(usuarios, edad){
-    var mayoresAEdad = usuarios.filter( function(numero){
-        return numero > 3;
+    return usuarios
+    .filter( function(user){
+        return user.edad > edad;
+    })
+    .map(function(user) {
+        return {
+            "nombre": user.nombre,
+            "email": user.email,
+            "edad": user.edad,
+        }
     });
-
 }
 
-//console.log(soluciones.usuariosMayores(data, 40));
+console.log(soluciones.usuariosMayores(data, 40));
 
-  // Retornar un objeto que contenga solo el nombre y la edad del usuario mas grande.
+// Retornar un objeto que contenga solo el nombre y la edad del usuario mas grande.
 
 soluciones.usuarioMasGrande = function(usuarios){
-    
+    return usuarios
+    .map(function(user){
+        return user.edad;
+    })
+    .reduce(function (previo, actual) {
+        if (previo > actual) {
+            return previo;
+        } else {
+            return actual;
+        }
+    });
 }
 
 //console.log(soluciones.usuarioMasGrande(data));
@@ -57,26 +74,54 @@ soluciones.usuarioMasGrande = function(usuarios){
 // Retornar el promedio de edad de los usuarios (number)
 
 soluciones.promedio = function(usuarios){
-  
-
+    var edades = usuarios.map(function(user){
+        return user.edad; 
+    })
+    var acumEdad = edades.reduce(function(previo,actual){
+        return previo + actual;
+    })
+    var cantidad = edades.reduce(function(previo,actual,index) {
+        return index;
+    })
     return (acumEdad /cantidad).toFixed(2);
 }
 
-//console.log("Promedio edad usuarios " + soluciones.promedio(data));
+console.log("Promedio edad usuarios " + soluciones.promedio(data));
 
 // Retornar el promedio de edad de los usuarios hombres (number)
 
 soluciones.promedioVarones = function(usuarios){
-   
-   
+    var arr = usuarios.filter(function(user){
+       return user.genero === 'Male';
+    })
+    var edadesHombre = arr.map(function(user){
+        return user.edad;
+    });
+    var acumEdadHombres = edadesHombre.reduce(function (previo,actual) {
+        return previo + actual;
+    })
+    var cantidadHombres = edadesHombre.length;
+
+    return (acumEdadHombres / cantidadHombres).toFixed(2);
 }
 
-//console.log("Promedio edad Varones " + soluciones.promedioVarones(data));
+console.log("Promedio edad Varones " + soluciones.promedioVarones(data));
 
  // Retornar el promedio de edad de los usuarios mujeres (number)
 
 soluciones.promedioMujeres = function(usuarios){
-   
+    var arr = usuarios.filter(function (user) {
+        return user.genero === 'Female';
+    })
+    var edadesMujeres = arr.map(function (user) {
+        return user.edad;
+    });
+    var acumEdadMujeres = edadesMujeres.reduce(function (previo, actual) {
+        return previo + actual;
+    })
+    var cantidadMujeres = edadesMujeres.length;
+
+    return (acumEdadMujeres / cantidadMujeres).toFixed(2);
 }
 
-//console.log("Promedio edad Mujeres " + soluciones.promedioMujeres(data));
+console.log("Promedio edad Mujeres " + soluciones.promedioMujeres(data));
